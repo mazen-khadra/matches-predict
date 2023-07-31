47f64cadeac80843812640c83aa4fe0323197dfc
Mazen ALkhadra <mazen.alkhadra@outlook.com>
2023-07-31 12:30:34 +0400

    Add Alt Sport Svc

--- app/services/TysonSports.php
+++ app/services/TysonSports.php
@@ -66,9 +66,9 @@ class TysonSports {
     return $res;
   }
 
-    function getMatches($sportId = null, $leagueId = null, $daysOffset = 0, $useAltSvc = true) : array {
+    function getMatches($sportId = null, $leagueId = null, $daysOffset = 0, $useAltSvc = false) : array {
         try {
-            if(!empty($useAltSvc)) {
+            if(!empty(true)) {
                 $queryParams = ["sportId" => $sportId, "hotLeague" => $leagueId, "daysOffset" => $daysOffset];
                 $queryParams = array_filter($queryParams, function($param) {return $param != null;});
                 $res = Http::withoutVerifying()->withHeaders($this->headers)->get (
