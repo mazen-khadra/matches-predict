f0bcd3e48120be8ad01e5dcf07f38aaa4ff3c5db
amjadsolutions <amjadjavedse@gmail.com>
2023-08-12 11:10:25 +0400

    updated code

--- app/Http/Controllers/Matches.php
+++ app/Http/Controllers/Matches.php
@@ -2,75 +2,79 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Services\TysonSport as SportAPI;
 use App\Http\Controllers\MatchPredict as MatchPredictController;
+use App\Services\TysonSport as SportAPI;
+use Illuminate\Http\Request;
 
 class Matches extends Controller
 {
-    public function index (
+    public function index(
         Request $req, $sport = null, $leagueId = null,
         $daysOffset = null, $useAltSvc = null
     ) {
-      $sport = $sport ?? $req->query('sport');
-      $leagueId = $leagueId ?? $req->query('leagueId');
-      $daysOffset = $daysOffset ?? $req->query('daysOffset');
-      $useAltSvc = $useAltSvc ?? $req->query('useAltSvc') ?? true;
-      $sportId = SportAPI::$SPORTS_IDS[$sport];
+        $sport = $sport ?? $req->query('sport');
+        $leagueId = $leagueId ?? $req->query('leagueId');
+        $daysOffset = $daysOffset ?? $req->query('daysOffset');
+        $useAltSvc = $useAltSvc ?? $req->query('useAltSvc') ?? true;
+        $sportId = SportAPI::$SPORTS_IDS[$sport];
 
-      $data = (new SportAPI())->getMatches (
-        $sportId, $leagueId, $daysOffset, $useAltSvc
-      );
+        $data = (new SportAPI())->getMatches(
+            $sportId, $leagueId, $daysOffset, $useAltSvc
+        );
 
-      $matchPredCont = new MatchPredictController();
+        $matchPredCont = new MatchPredictController();
 
-      foreach($data as &$match) {
-          $predStats = $matchPredCont->getMatchStats($match['id']);
-          $homeTeamCnt = 0;
-          $awayTeamCnt = 0;
-          $match["pred_stats"] = $predStats;
-          foreach ($predStats["winStats"] as $stat) {
-              if($stat["team_id"] == $match["home_team_id"])
-                  $homeTeamCnt = $stat["cnt"];
-              elseif($stat["team_id"] == $match["away_team_id"])
-                  $awayTeamCnt = $stat["cnt"];
-          }
+        foreach ($data as &$match) {
+            $predStats = $matchPredCont->getMatchStats($match['id']);
+            $homeTeamCnt = 0;
+            $awayTeamCnt = 0;
+            $match["pred_stats"] = $predStats;
+            foreach ($predStats["winStats"] as $stat) {
+                if ($stat["team_id"] == $match["home_team_id"]) {
+                    $homeTeamCnt = $stat["cnt"];
+                } elseif ($stat["team_id"] == $match["away_team_id"]) {
+                    $awayTeamCnt = $stat["cnt"];
+                }
 
-          $match["pred_stats"]["winStats"] = ["home_team_cnt" => $homeTeamCnt, "away_team_cnt" => $awayTeamCnt];
-      }
+            }
 
-      return $data;
+            $match["pred_stats"]["winStats"] = ["home_team_cnt" => $homeTeamCnt, "away_team_cnt" => $awayTeamCnt];
+        }
+
+        return $data;
     }
 
-    public function details(Request $req, $matchId) {
-      $user = $req->user('sanctum');
-      $userId = !empty($user) ? $user->id : null;
-      $data = (new SportAPI())->getMatchDetails($matchId);
-      $predStats = (new MatchPredictController())->index($req, false, $matchId);
-      $finalPredStats = ["home" => [], "away" => [], "draws" => []];
+    public function details(Request $req, $matchId)
+    {
+        $user = $req->user('sanctum');
+        $userId = !empty($user) ? $user->id : null;
+        $data = (new SportAPI())->getMatchDetails($matchId);
+        $predStats = (new MatchPredictController())->index($req, false, $matchId);
+        $finalPredStats = ["home" => [], "away" => [], "draws" => []];
 
+        foreach ($predStats as $pred) {
+            $userInfo = [
+                "name" => $pred->user["name"],
+                "img_url" => $pred->user["img_url"],
+                "pred_stats" => $pred->user["pred_stats"],
+            ];
+            if ($pred["user_id"] == $userId) {
+                $finalPredStats['current_user'] = [
+                    "for_home" => $pred["winner_team_id"] == $data["home_team_id"],
+                    "for_away" => $pred["winner_team_id"] == $data["away_team_id"],
+                    "draw" => boolval($pred["draw"]),
+                    "is_success" => boolval($pred["is_success"]),
+                ];
+            } else if ($pred["draw"]) {
+                $finalPredStats["draws"][] = $userInfo;
+            } else if ($pred["winner_team_id"] == $data["home_team_id"]) {
+                $finalPredStats["home"][] = $userInfo;
+            } else if ($pred["winner_team_id"] == $data["away_team_id"]) {
+                $finalPredStats["away"][] = $userInfo;
+            }
 
-      foreach ($predStats as $pred) {
-          $userInfo = [
-              "name" => $pred->user["name"],
-              "img_url" => $pred->user["img_url"],
-              "pred_stats" => $pred->user["pred_stats"]
-          ];
-          if($pred["user_id"] == $userId)
-              $finalPredStats['current_user'] = [
-                  "for_home" => $pred["winner_team_id"] == $data["home_team_id"],
-                  "for_away" => $pred["winner_team_id"] == $data["away_team_id"],
-                  "draw" => boolval($pred["draw"]),
-                  "is_success" => boolval($pred["is_success"])
-              ];
-          else if($pred["draw"])
-              $finalPredStats["draws"][] = $userInfo;
-          else if($pred["winner_team_id"] == $data["home_team_id"])
-              $finalPredStats["home"][] = $userInfo;
-          else if($pred["winner_team_id"] == $data["away_team_id"])
-              $finalPredStats["away"][] = $userInfo;
-      }
-      $data["pred_stats"] = $finalPredStats;
-      return $data;
+        }
+        $data["pred_stats"] = $finalPredStats;
+        return $data;
     }
 }
--- app/Http/Controllers/User.php
+++ app/Http/Controllers/User.php
@@ -2,27 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\User as UserModel;
 use App\Http\Controllers\Img as ImgController;
-Use App\Models\Img as ImgModel;
+use App\Models\Img as ImgModel;
+use App\Models\User as UserModel;
+use Illuminate\Http\Request;
 
 class User extends Controller
 {
-    public function index (Request $req) {
-        $userId = $req->user()->id;
-
-        return UserModel::whereNot('id', $userId)->get();
+    public function index(Request $req)
+    {
+        echo "Amjad Javed";
+        // $userId = $req->user()->id;
+        // return UserModel::whereNot('id', $userId)->get();
     }
 
-    public function updateProfile(Request $req) {
+    public function updateProfile(Request $req)
+    {
         $userId = $req->user()->id;
         $data = [];
 
-        if(!empty($req->name))
-          $data["name"] = $req->name;
+        if (!empty($req->name)) {
+            $data["name"] = $req->name;
+        }
 
-        if($req->hasFile('img')) {
+        if ($req->hasFile('img')) {
             $data["img_id"] = ImgModel::getImgIdByUrl((new ImgController())->uploadImg($req));
         }
 
