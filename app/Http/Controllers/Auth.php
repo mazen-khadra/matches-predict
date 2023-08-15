08dd24edff50663a52424ce8495ef88a85fcc4ff
amjadsolutions <amjadjavedse@gmail.com>
2023-08-15 11:30:49 +0400

    updated code

--- app/Http/Controllers/Auth.php
+++ app/Http/Controllers/Auth.php
@@ -2,35 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\User as UserModel;
 use App\Models\Img as ImgModel;
+use App\Models\User as UserModel;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth as AuthFacade;
 
 class Auth extends Controller
 {
-    public function signUp(Request $req) {
+    public function signUp(Request $req)
+    {
         $data = $req->validate([
             "name" => 'bail|required|alpha',
-            "email" => 'bail|required|email|unique:users',
-            "password" => 'required'
+            "email" => 'bail|required|email|:users',
+            "password" => 'required',
         ]);
 
-        if(!empty($req->img_url))
+        if (!empty($req->img_url)) {
             $data["img_id"] = ImgModel::getImgIdByUrl($req->img_url);
+        }
+        if (UserModel::where('email', '=', $req->email)->count() == 0) {
+            UserModel::create($data);
+            return $this->logIn($req);
+        }
 
-        UserModel::create($data);
-
-        return $this->logIn($req);
+        return response(["error" => "The email has already been taken"], 401);
     }
 
-    public function logIn(Request $req) {
+    public function logIn(Request $req)
+    {
         $data = $req->validate([
             "email" => "required|email",
-            "password" => "required"
+            "password" => "required",
         ]);
 
-        if(AuthFacade::attempt($data)) {
+        if (AuthFacade::attempt($data)) {
             $user = AuthFacade::user();
             $token = $user->createToken($user->id)->plainTextToken;
             $user->token = $token;
@@ -40,14 +45,17 @@ class Auth extends Controller
         return response(["error" => "Invalid Credentials"], 401);
     }
 
-    public function getLoggedInUser() {
-        if(AuthFacade::check())
+    public function getLoggedInUser()
+    {
+        if (AuthFacade::check()) {
             return AuthFacade::user();
+        }
 
         return response(null, 401);
     }
 
-    public function logOut() {
+    public function logOut()
+    {
         AuthFacade::logout();
     }
 }
